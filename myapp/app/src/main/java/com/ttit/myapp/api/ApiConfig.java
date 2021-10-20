abe73fcbcb5e1eda98e66f7f9feef984f0282b3a
zuoxianglei <1608808241@qq.com>
2021-10-20 22:56:21 +0800

    提交app

--- myapp/app/src/main/java/com/ttit/myapp/api/ApiConfig.java
+++ myapp/app/src/main/java/com/ttit/myapp/api/ApiConfig.java
@@ -2,7 +2,7 @@ package com.ttit.myapp.api;
 
 public class ApiConfig {
     public static final int PAGE_SIZE = 5;
-    public static final String BASE_URl = "http://192.168.31.32:8080/renren-fast";
+    public static final String BASE_URl = "http://10.0.0.2:8080/renren-fast";
     public static final String LOGIN = "/app/login"; //登录
     public static final String REGISTER = "/app/register";//注册
     public static final String VIDEO_LIST_ALL = "/app/videolist/listAll";//所有类型视频列表
